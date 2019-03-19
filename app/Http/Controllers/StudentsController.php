09310b1daf25ed67fd3eff453168e718605ac6b6
Mirko Dzudzar <mirkodzudzar@gmail.com>
2019-03-19 16:32:44 +0100

    Adding middleware 19.03.2019.

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -12,6 +12,16 @@ use DB;
 class StudentsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
